a227331b79b29ac4d2fce350bcf44f8bcbed5033
Gabriel Messias <gamessiasjt@gmail.com>
2023-07-25 21:13:24 -0300

    Tipos enumerados criados

---
+++ src/main/java/com/messias/helpdesk/helpdeskproject/entities/enums/Profile.java
@@ -0,0 +1,33 @@
+package com.messias.helpdesk.helpdeskproject.entities.enums;
+
+public enum Profile {
+	ADMIN(0, "ROLE_ADMIN"), CLIENT(1, "ROLE_CLIENT"), TECHNICIAN(2, "ROLE_TECHNICIAN");
+
+	private Integer code;
+	private String description;
+
+	private Profile(Integer code, String description) {
+		this.code = code;
+		this.description = description;
+	}
+
+	public Integer getCode() {
+		return code;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public static Profile toEnum(Integer code) {
+		if(code == null) {
+			return null;
+		}
+		for(Profile x : Profile.values()) {
+			if(code.equals(x.getCode())) {
+				return x;
+			}
+		}
+		 throw new IllegalArgumentException("This code to profile is invalid");
+	}
+}
